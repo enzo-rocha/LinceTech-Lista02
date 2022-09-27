7220e5b1c073f5862580ee6d2d21ddc9819a09b8
EnzoRocha <106124179+enzo-rocha@users.noreply.github.com>
2022-09-27 07:57:21 -0300

    Create ex01.dart

---
+++ ex01.dart
@@ -0,0 +1,53 @@
+// NÃO PODE SER MODIFICADO
+// -------------------------------------------------------------
+import 'dart:math' as math;
+
+void main() {
+  final meioDeComunicacao = aleatorio();
+  meioDeComunicacao.fazerLigacao('(47) 99876-5432');
+}
+
+MeioDeComunicacao aleatorio() {
+  final meiosDeComunicacao = <MeioDeComunicacao>[
+    Telefone(),
+    Celular(),
+    Orelhao(),
+  ];
+
+  final random = math.Random();
+
+  return meiosDeComunicacao[random.nextInt(
+    meiosDeComunicacao.length,
+  )];
+}
+
+// -------------------------------------------------------------
+// ADICIONAR IMPLEMENTAÇÃO RESTANTE ABAIXO DESSA LINHA
+// -------------------------------------------------------------
+
+class MeioDeComunicacao {
+  void fazerLigacao(String numero) {
+    
+  }
+}
+
+class Telefone extends MeioDeComunicacao {
+  @override
+  void fazerLigacao(String numero) {
+    print("[TELEFONE] ligando para $numero");
+  }
+}
+
+class Celular extends MeioDeComunicacao {
+  @override
+  void fazerLigacao(String numero) {
+    print("[CELULAR] ligando para $numero");
+  }
+}
+
+class Orelhao extends MeioDeComunicacao {
+  @override
+  void fazerLigacao(String numero) {
+    print("[ORELHAO] ligando para $numero");
+  }
+}
