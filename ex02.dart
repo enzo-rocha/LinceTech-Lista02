4fb5cf241a0c2acfa9fe7254e2bdbefe2a1ca957
EnzoRocha <106124179+enzo-rocha@users.noreply.github.com>
2022-09-27 21:03:28 -0300

    Update ex02.dart

--- ex02.dart
+++ ex02.dart
@@ -2,19 +2,38 @@ import 'dart:math';
 
 void main() {
    final pessoa = Pessoa();
-   final randomFornecedor = Random();
-   final fornecedor;
+   final randomFornecedor = Random().nextInt(6);
+   final fornecedorBebidas = FornecedorDeBebidas();
+   final fornecedorSanduiches = FornecedorDeSanduiches();
+   final fornecedorBolos = FornecedorDeBolos();
+   final fornecedorSaladas = FornecedorDeSaladas();
+   final fornecedorPetiscos = FornecedorDePetiscos();
+   final fornecedorUltras = FornecedorDeUltras();
   
-  if (randomFornecedor.nextInt(2) == 1) {
-    fornecedor = FornecedorDeBebidas();
+  if (randomFornecedor == 0) {
     for (var i = 0; i < 5; i++) {
-      pessoa.refeicaoBebidas(fornecedor);
-   }
-  } else if (randomFornecedor.nextInt(2) == 2) {
-    fornecedor = FornecedorDeSanduiches();
+      pessoa.refeicaoBebidas(fornecedorBebidas);
+    }
+  } else if (randomFornecedor == 1) {
     for (var i = 0; i < 5; i++) {
-      pessoa.refeicaoSanduiches(fornecedor);
-   }
+      pessoa.refeicaoSanduiches(fornecedorSanduiches);
+    }
+  } else if (randomFornecedor == 2) {
+    for (var i = 0; i < 5; i++) {
+      pessoa.refeicaoBolos(fornecedorBolos);
+    }
+  } else if (randomFornecedor == 3) {
+    for (var i = 0; i < 5; i++) {
+      pessoa.refeicaoSaladas(fornecedorSaladas);
+    }
+  } else if (randomFornecedor == 4) {
+    for (var i = 0; i < 5; i++) {
+      pessoa.refeicaoPetiscos(fornecedorPetiscos);
+    }
+  } else if (randomFornecedor == 5) {
+    for (var i = 0; i < 5; i++) {
+      pessoa.refeicaoUltras(fornecedorUltras);
+    }
   }
 
    pessoa.informacoes();
@@ -38,11 +57,11 @@ class FornecedorDeBebidas {
       Produto('Suco de fruta', 100),
       Produto('Energetico', 135),
       Produto('Café', 60),
-      Produto('Cha', 35),
+      Produto('Cha', 35)
    ];
 
    /// Retorna um produto que pode ser consumido por um consumidor
-   Produto fornecer() {
+   Produto fornecerBebidas() {
       return _bebidasDisponiveis[_random.nextInt(_bebidasDisponiveis.length)];
    }
 }
@@ -50,17 +69,85 @@ class FornecedorDeBebidas {
 class FornecedorDeSanduiches {
   final _random = Random();
    final _sanduichesDisponiveis = <Produto>[
-      Produto('Sanduiche de presunto', 250),
+      Produto('Sanduiche de presunto', 200),
       Produto('Sanduiche integral', 100),
-      Produto('Sanduiche de mostarda', 150),
-      Produto('Sanduiche de carne', 300),
-      Produto('Sanduiche de peito de peru', 250),
+      Produto('Sanduiche de mostarda', 130),
+      Produto('Sanduiche de carne', 200),
+      Produto('Sanduiche de peito de peru', 180),
       Produto('Sanduiche de mortadela', 200)
    ];
 
    /// Retorna um produto que pode ser consumido por um consumidor
    Produto fornecerSanduiches() {
       return _sanduichesDisponiveis[_random.nextInt(_sanduichesDisponiveis.length)];
+   }
+}
+
+class FornecedorDeBolos {
+  final _random = Random();
+   final _bolosDisponiveis = <Produto>[
+      Produto('Bolo de morango', 130),
+      Produto('Bolo de chocolate', 230),
+      Produto('Bolo de nozes', 100),
+      Produto('Bolo de Nutella', 140),
+      Produto('Bolo de cenoura', 340),
+      Produto('Bolo de fubá', 100)
+   ];
+
+   /// Retorna um produto que pode ser consumido por um consumidor
+   Produto fornecerBolos() {
+      return _bolosDisponiveis[_random.nextInt(_bolosDisponiveis.length)];
+   }
+}
+
+class FornecedorDeSaladas {
+  final _random = Random();
+   final _saladasDisponiveis = <Produto>[
+      Produto('Salada de alface', 14),
+      Produto('Salada de tomate', 40),
+      Produto('Salada de agrião', 30),
+      Produto('Salada de batata', 80),
+      Produto('Salada de cebola', 35),
+      Produto('Salada de abóbora', 40)
+   ];
+
+   /// Retorna um produto que pode ser consumido por um consumidor
+   Produto fornecerSaladas() {
+      return _saladasDisponiveis[_random.nextInt(_saladasDisponiveis.length)];
+   }
+}
+
+class FornecedorDePetiscos {
+  final _random = Random();
+   final _petiscosDisponiveis = <Produto>[
+      Produto('Amendoim', 100),
+      Produto('Castanha', 80),
+      Produto('Pipoca', 40),
+      Produto('Peixe frito', 100),
+      Produto('Ovo de codorna', 80),
+      Produto('Azeitona', 40)
+   ];
+
+   /// Retorna um produto que pode ser consumido por um consumidor
+   Produto fornecerPetiscos() {
+      return _petiscosDisponiveis[_random.nextInt(_petiscosDisponiveis.length)];
+   }
+}
+
+class FornecedorDeUltras {
+  final _random = Random();
+   final _ultrasDisponiveis = <Produto>[
+      Produto('Bacon', 150),
+      Produto('Chocolate', 150),
+      Produto('Hamburguer', 250),
+      Produto('Batata frita', 150),
+      Produto('Pizza', 400),
+      Produto('Pastel', 250)
+   ];
+
+   /// Retorna um produto que pode ser consumido por um consumidor
+   Produto fornecerUltras() {
+      return _ultrasDisponiveis[_random.nextInt(_ultrasDisponiveis.length)];
    }
 }
 
@@ -74,17 +161,45 @@ class Pessoa {
    }
 
    /// Consome um produto e aumenta o numero de calorias
-   void refeicaoBebidas(FornecedorDeBebidas fornecedor) {
-      final produto01 = fornecedor.fornecer();
-      print('Consumindo ${produto01.nome} (${produto01.calorias} calorias)');
+   void refeicaoBebidas(FornecedorDeBebidas fornecedorBebidas) {
+      final produto = fornecedorBebidas.fornecerBebidas();
+      print('Consumindo ${produto.nome} (${produto.calorias} calorias)');
 
-      _caloriasConsumidas += produto01.calorias;
+      _caloriasConsumidas += produto.calorias;
    }
   
-  void refeicaoSanduiches(FornecedorDeSanduiches fornecedor) {
-      final produto02 = fornecedor.fornecerSanduiches();
-      print('Consumindo ${produto02.nome} (${produto02.calorias} calorias)');
+  void refeicaoSanduiches(FornecedorDeSanduiches fornecedorSanduiches) {
+      final produto = fornecedorSanduiches.fornecerSanduiches();
+      print('Consumindo ${produto.nome} (${produto.calorias} calorias)');
 
-      _caloriasConsumidas += produto02.calorias;
+      _caloriasConsumidas += produto.calorias;
+   }
+  
+  void refeicaoBolos(FornecedorDeBolos fornecedorBolos) {
+      final produto = fornecedorBolos.fornecerBolos();
+      print('Consumindo ${produto.nome} (${produto.calorias} calorias)');
+
+      _caloriasConsumidas += produto.calorias;
+   }
+  
+  void refeicaoSaladas(FornecedorDeSaladas fornecedorSaladas) {
+      final produto = fornecedorSaladas.fornecerSaladas();
+      print('Consumindo ${produto.nome} (${produto.calorias} calorias)');
+
+      _caloriasConsumidas += produto.calorias;
+   }
+  
+  void refeicaoPetiscos(FornecedorDePetiscos fornecedorPetiscos) {
+      final produto = fornecedorPetiscos.fornecerPetiscos();
+      print('Consumindo ${produto.nome} (${produto.calorias} calorias)');
+
+      _caloriasConsumidas += produto.calorias;
+   }
+  
+  void refeicaoUltras(FornecedorDeUltras fornecedorUltras) {
+      final produto = fornecedorUltras.fornecerUltras();
+      print('Consumindo ${produto.nome} (${produto.calorias} calorias)');
+
+      _caloriasConsumidas += produto.calorias;
    }
 }
