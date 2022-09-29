a9743f2b51f82d58742d2b7e0bac5f7817c01d8e
EnzoRocha <106124179+enzo-rocha@users.noreply.github.com>
2022-09-29 20:29:28 -0300

    Update ex02.dart

--- ex02.dart
+++ ex02.dart
@@ -35,8 +35,19 @@ void main() {
       pessoa.refeicaoUltras(fornecedorUltras);
     }
   }
-
+  
+   pessoa.mostrarCaloriasIniciais();
+  
    pessoa.informacoes();
+  
+   pessoa.totalCalorias();
+  
+   pessoa.mostrarStatus();
+  
+   pessoa.precisaRefeicoes();
+  
+   pessoa.numeroDeRefeicoes();
+  
 }
 
 class Produto {
@@ -137,12 +148,12 @@ class FornecedorDePetiscos {
 class FornecedorDeUltras {
   final _random = Random();
    final _ultrasDisponiveis = <Produto>[
-      Produto('Bacon', 150),
-      Produto('Chocolate', 150),
-      Produto('Hamburguer', 250),
-      Produto('Batata frita', 150),
+      Produto('Bacon', 400),
+      Produto('Chocolate', 300),
+      Produto('Hamburguer', 500),
+      Produto('Batata frita', 300),
       Produto('Pizza', 400),
-      Produto('Pastel', 250)
+      Produto('Pastel', 500)
    ];
 
    /// Retorna um produto que pode ser consumido por um consumidor
@@ -152,54 +163,188 @@ class FornecedorDeUltras {
 }
 
 class Pessoa {
-   // Acumulador de calorias
+   
+   late Status status;
+  
+   int numeroRefeicoes = 0;
+  
+  // Acumulador de calorias
+  
    int _caloriasConsumidas = 0;
+ 
+   // Gera calorias iniciais aleatorimente
+   final int _caloriasInicial = Random().nextInt(1000);
+  
+   int getTotalCalorias() {
+     int totalDeCalorias = _caloriasConsumidas + _caloriasInicial;
+     return totalDeCalorias;
+   }
+  
+   //
+   void totalCalorias() {
+     print("Total de calorias da pessoa após refeição: ${_caloriasConsumidas + _caloriasInicial}");
+   }
 
    /// Imprime as informações desse consumidor
    void informacoes() {
       print('Calorias consumidas: $_caloriasConsumidas');
    }
+  
+  void mostrarCaloriasIniciais() {
+    print("Calorias iniciais: $_caloriasInicial");
+  }
 
-   /// Consome um produto e aumenta o numero de calorias
+   /// Consome uma bebida e aumenta o numero de calorias
    void refeicaoBebidas(FornecedorDeBebidas fornecedorBebidas) {
       final produto = fornecedorBebidas.fornecerBebidas();
       print('Consumindo ${produto.nome} (${produto.calorias} calorias)');
 
       _caloriasConsumidas += produto.calorias;
+     
+      if (getTotalCalorias() <= 500) {
+          status = Status.deficitExtremo;
+      } else if (getTotalCalorias() > 500 && getTotalCalorias() <= 1800) {
+          status = Status.deficit;
+      } else if (getTotalCalorias() > 1800 && getTotalCalorias() <= 2500) {
+          status = Status.satisfeito;
+      } else if (getTotalCalorias() > 2500) {
+          status = Status.excesso;
+      }
    }
   
+  /// Consome um sanduiche e aumenta o numero de calorias
   void refeicaoSanduiches(FornecedorDeSanduiches fornecedorSanduiches) {
       final produto = fornecedorSanduiches.fornecerSanduiches();
       print('Consumindo ${produto.nome} (${produto.calorias} calorias)');
 
       _caloriasConsumidas += produto.calorias;
+    
+      if (getTotalCalorias() <= 500) {
+          status = Status.deficitExtremo;
+      } else if (getTotalCalorias() > 500 && getTotalCalorias() <= 1800) {
+          status = Status.deficit;
+      } else if (getTotalCalorias() > 1800 && getTotalCalorias() <= 2500) {
+          status = Status.satisfeito;
+      } else if (getTotalCalorias() > 2500) {
+          status = Status.excesso;
+      }
    }
   
+  /// Consome um bolo e aumenta o numero de calorias
   void refeicaoBolos(FornecedorDeBolos fornecedorBolos) {
       final produto = fornecedorBolos.fornecerBolos();
       print('Consumindo ${produto.nome} (${produto.calorias} calorias)');
 
       _caloriasConsumidas += produto.calorias;
+    
+      if (getTotalCalorias() <= 500) {
+          status = Status.deficitExtremo;
+      } else if (getTotalCalorias() > 500 && getTotalCalorias() <= 1800) {
+          status = Status.deficit;
+      } else if (getTotalCalorias() > 1800 && getTotalCalorias() <= 2500) {
+          status = Status.satisfeito;
+      } else if (getTotalCalorias() > 2500) {
+          status = Status.excesso;
+      }
    }
   
+  /// Consome uma salada e aumenta o numero de calorias
   void refeicaoSaladas(FornecedorDeSaladas fornecedorSaladas) {
       final produto = fornecedorSaladas.fornecerSaladas();
       print('Consumindo ${produto.nome} (${produto.calorias} calorias)');
 
       _caloriasConsumidas += produto.calorias;
+    
+      if (getTotalCalorias() <= 500) {
+          status = Status.deficitExtremo;
+      } else if (getTotalCalorias() > 500 && getTotalCalorias() <= 1800) {
+          status = Status.deficit;
+      } else if (getTotalCalorias() > 1800 && getTotalCalorias() <= 2500) {
+          status = Status.satisfeito;
+      } else if (getTotalCalorias() > 2500) {
+          status = Status.excesso;
+      }
    }
   
+  /// Consome um petisco e aumenta o numero de calorias
   void refeicaoPetiscos(FornecedorDePetiscos fornecedorPetiscos) {
       final produto = fornecedorPetiscos.fornecerPetiscos();
       print('Consumindo ${produto.nome} (${produto.calorias} calorias)');
 
       _caloriasConsumidas += produto.calorias;
+    
+      if (getTotalCalorias() <= 500) {
+          status = Status.deficitExtremo;
+      } else if (getTotalCalorias() > 500 && getTotalCalorias() <= 1800) {
+          status = Status.deficit;
+      } else if (getTotalCalorias() > 1800 && getTotalCalorias() <= 2500) {
+          status = Status.satisfeito;
+      } else if (getTotalCalorias() > 2500) {
+          status = Status.excesso;
+      }
    }
   
+  /// Consome um ultra calorico e aumenta o numero de calorias
   void refeicaoUltras(FornecedorDeUltras fornecedorUltras) {
       final produto = fornecedorUltras.fornecerUltras();
       print('Consumindo ${produto.nome} (${produto.calorias} calorias)');
 
       _caloriasConsumidas += produto.calorias;
+    
+      if (getTotalCalorias() <= 500) {
+          status = Status.deficitExtremo;
+      } else if (getTotalCalorias() > 500 && getTotalCalorias() <= 1800) {
+          status = Status.deficit;
+      } else if (getTotalCalorias() > 1800 && getTotalCalorias() <= 2500) {
+          status = Status.satisfeito;
+      } else if (getTotalCalorias() > 2500) {
+          status = Status.excesso;
+      }
    }
+  
+   void mostrarStatus() {
+     switch (status){
+       case Status.deficitExtremo:
+         print("Status: Deficit extremo de calorias");
+         break;
+       case Status.deficit:
+         print("Status: Deficit de calorias");
+         break;
+       case Status.satisfeito:
+         print("Status: Pessoa satisfeita");
+         break;
+       case Status.excesso:
+         print("Status: Excesso de calorias");
+     }
+   }
+  
+    void precisaRefeicoes() {
+      switch (status){
+       case Status.deficitExtremo:
+         print("Precisa de mais refeições");
+          numeroRefeicoes++;
+         break;
+       case Status.deficit:
+         print("Precisa de mais refeições");
+          numeroRefeicoes++;
+         break;
+       case Status.satisfeito:
+         print("Não precisa de mais refeições");
+          numeroRefeicoes++;
+         break;
+       case Status.excesso:
+         print("Não precisa de mais refeições");
+          numeroRefeicoes++;
+     }
+    }
+  
+   void numeroDeRefeicoes() => print("Numero de refeições: $numeroRefeicoes");
+  
+}
+
+enum Status {
+  deficitExtremo,
+  deficit,
+  satisfeito,
+  excesso,
 }
